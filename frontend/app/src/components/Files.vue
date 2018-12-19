86167d3b67b4b50bb8406eb4f617b7f047b2b6d5
Ivan Kuzma <vanikuzma911@gmail.com>
2018-12-19 18:09:11 +0200

    impl

--- frontend/app/src/components/Files.vue
+++ frontend/app/src/components/Files.vue
@@ -3,12 +3,18 @@
         <ul id="example-1" class="list">
           <li v-for="(file, index) in files" v-bind:key="index">
             {{ file.name }}
+            <input type="password" v-model="passwords[index]" />
+            <img v-if="file.file" v-bind:src="file.file" />
+            {{file.file}}
+            <button v-on:click="viewFile(index, file.id)" >View</button>
+            <button v-on:click="deleteFile(file)" >x</button>
         </li>
       </ul>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import config from '../config'
 import File from './File'
 
@@ -19,7 +25,8 @@ export default {
   },
   data () {
     return {
-      files: []
+      files: [],
+      passwords: []
     }
   },
   created () {
@@ -43,6 +50,54 @@ export default {
         .catch(e => {
           console.error(e)
         })
+    },
+    viewFile (index, fileId) {
+      const secret = this.passwords[index]
+
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          token: localStorage.getItem('token')
+        },
+        body: JSON.stringify({ secret })
+      }
+
+      fetch(`${config.apiUrl}/file/${fileId}`, requestOptions)
+        .then(r => r.json())
+        .then(result => {
+          const findIndex = this.files.findIndex(({id}) => id === fileId)
+          if (findIndex >= 0) {
+            // const files = this.files
+            // //@TODO: add watch method
+            // //force replace all files
+            // files[findIndex] = { ...files[findIndex], file: `data:image/png;base64,${result.file}` }
+            // this.files = {...files}
+            const newFileData = { ...this.files[findIndex], file: `data:image/png;base64,${result.file}` }
+            Vue.set(this.files, findIndex, newFileData)
+          }
+        })
+        .catch(e => {
+          console.error(e)
+        })
+    },
+    deleteFile ({ id: deletedId }) {
+      const requestOptions = {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          token: localStorage.getItem('token')
+        }
+      }
+
+      fetch(`${config.apiUrl}/file/${deletedId}`, requestOptions)
+        .then(r => {
+          this.files = this.files.filter(({id}) => id !== deletedId)
+          console.log(r)
+        })
+        .catch(e => {
+          console.error(e)
+        })
     }
 
   }
@@ -56,7 +111,7 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
-  width: 30%;
+  width: 50%;
   float: left;
   border: 1px solid grey;
   font-size: 25px;
